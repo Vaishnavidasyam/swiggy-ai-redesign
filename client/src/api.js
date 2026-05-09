7cc5185a8faf89a89496f092f53999ea5651af41
Vaishnavidasyam <vaishnavi.41469@gmail.com>
2026-05-10 02:24:39 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://swiggy-ai-redesign.onrender.com",
+  baseURL: "https://swiggy-ai-redesign.onrender.com/api",
 });
 
 export default api;
