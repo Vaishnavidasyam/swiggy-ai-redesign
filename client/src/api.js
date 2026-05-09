4c16ccc51ec41defd81d93eca8f512568e95f66f
Vaishnavidasyam <vaishnavi.41469@gmail.com>
2026-05-10 04:01:09 +0530

    Added AI cart checkout demo

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
+<<<<<<< HEAD
   baseURL: "https://swiggy-ai-redesign.onrender.com/api",
+=======
+  baseURL: "https://swiggy-ai-redesign.onrender.com",
+>>>>>>> 238acd8 (Added Vercel routing fix)
 });
 
 export default api;
